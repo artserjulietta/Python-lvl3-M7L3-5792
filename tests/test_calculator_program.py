f0f1021483487530999c2109987f4d8992793120
Psiholog <timofei.motoshin@yandex.ru>
2024-03-30 17:07:43 +0300

    afafawfawf

--- tests/test_calculator_program.py
+++ tests/test_calculator_program.py
@@ -7,6 +7,11 @@ def test_calculate_addition():
 def test_calculate_division():
     assert calculate(8, 2, '/') == 4
 
+def test_calculate_subtraction():
+    assert calculate(4, 2, '-') == 2
+def test_calculate_multiplication():
+    assert calculate(2, 3, '*') == 6
+
 def test_calculate_unknown_operation():
     assert calculate(5, 5, 'unknown') == "Неизвестная операция."
 
